380668aeba9222cf1e3ad9b9901f00b1227c1613
Tehaunui Shing Soi <150084144+znxtgen@users.noreply.github.com>
2023-12-06 10:48:38 +1300

    Section 9 JS&CSS

--- shared.js
+++ shared.js
@@ -1,25 +1,53 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
-var selectNo = document.querySelector('.modal__action--negative')
+var selectNo = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav')
 
-// console.dir(backdrop);
-console.log(selectNo);
+// console.dir(backdrop.style['background-image']);
+// console.log(selectNo);
+
 
 /** Add event listener **/
 // Loop through the array
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function () {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = "open"; // This will ovewrite the complete class list
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     })
 }
 
 /** Closes modal if click NO **/
-backdrop.addEventListener('click', closeModal);
-selectNo.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function () {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
 
+if (selectNo) {
+    selectNo.addEventListener('click', closeModal);
+}
+
+
+/** Toggle btn event listener **/
+toggleButton.addEventListener('click', function () {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
+
+
+/****** Functions *****/
 function closeModal() {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
 }
